d87fcdc1f940286fe74cc92412656d91fc9023b4
jualiana-kiro <julibernardo12@gmail.com>
2026-06-06 00:54:45 -0300

    design: integrate roses and carnations in cover and soften background plants to pop the name

--- components/invitation/HeroSection.tsx
+++ components/invitation/HeroSection.tsx
@@ -20,12 +20,29 @@ export default function HeroSection() {
         <div className="relative w-full max-w-4xl mx-auto flex items-center justify-center my-8 sm:my-12 py-10 select-none">
           
           {/* ========================================================
-              CAPA DE FONDO (z-0) - Lecho floral denso detrás del texto
+              CAPA DE FONDO (z-0) - Lecho floral denso detrás del texto (Opción 2)
               ======================================================== */}
           <div className="absolute inset-0 z-0 flex items-center justify-center pointer-events-none">
             
-            {/* 1. Centro-Izquierda Fondo (Grupo03.png) */}
-            <div className="absolute left-[25%] sm:left-[30%] top-1/2 -translate-y-1/2 -translate-x-1/2 w-40 h-40 sm:w-60 sm:h-60 md:w-72 md:h-72 opacity-95">
+            {/* 1. Centro Fondo - Rosas y claveles especiales (FloresEspeciales.png) */}
+            <div 
+              className="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 w-80 h-48 sm:w-[500px] sm:h-[300px] md:w-[600px] md:h-[360px] opacity-75 z-0"
+              style={{ mixBlendMode: "multiply", filter: "brightness(0.95) contrast(0.90)" }}
+            >
+              <div className="w-full h-full animate-sway-slow">
+                <img
+                  src="/Flores/FloresEspeciales.png"
+                  alt=""
+                  className="w-full h-full object-contain"
+                />
+              </div>
+            </div>
+
+            {/* 2. Centro-Izquierda Fondo (Grupo03.png) */}
+            <div 
+              className="absolute left-[25%] sm:left-[30%] top-1/2 -translate-y-1/2 -translate-x-1/2 w-40 h-40 sm:w-60 sm:h-60 md:w-72 md:h-72 opacity-75"
+              style={{ filter: "brightness(0.95) contrast(0.90)" }}
+            >
               <div className="w-full h-full animate-sway-slow">
                 <img
                   src="/Flores/Grupo03.png"
@@ -35,8 +52,11 @@ export default function HeroSection() {
               </div>
             </div>
 
-            {/* 2. Centro-Derecha Fondo (Grupo03.png invertido) */}
-            <div className="absolute right-[25%] sm:right-[30%] top-1/2 -translate-y-1/2 translate-x-1/2 w-40 h-40 sm:w-60 sm:h-60 md:w-72 md:h-72 opacity-95">
+            {/* 3. Centro-Derecha Fondo (Grupo03.png invertido) */}
+            <div 
+              className="absolute right-[25%] sm:right-[30%] top-1/2 -translate-y-1/2 translate-x-1/2 w-40 h-40 sm:w-60 sm:h-60 md:w-72 md:h-72 opacity-75"
+              style={{ filter: "brightness(0.95) contrast(0.90)" }}
+            >
               <div className="w-full h-full animate-sway-slow">
                 <img
                   src="/Flores/Grupo03.png"
@@ -46,8 +66,11 @@ export default function HeroSection() {
               </div>
             </div>
 
-            {/* 3. Extremo Izquierda Fondo (Grupo02_c.png) */}
-            <div className="absolute left-[8%] sm:left-[12%] top-[45%] -translate-y-1/2 -translate-x-1/2 w-32 h-32 sm:w-48 sm:h-48 md:w-56 md:h-56 opacity-90">
+            {/* 4. Extremo Izquierda Fondo (Grupo02_c.png) */}
+            <div 
+              className="absolute left-[8%] sm:left-[12%] top-[45%] -translate-y-1/2 -translate-x-1/2 w-32 h-32 sm:w-48 sm:h-48 md:w-56 md:h-56 opacity-75"
+              style={{ filter: "brightness(0.95) contrast(0.90)" }}
+            >
               <div className="w-full h-full animate-sway-left">
                 <img
                   src="/Flores/Grupo02_c.png"
@@ -57,8 +80,11 @@ export default function HeroSection() {
               </div>
             </div>
 
-            {/* 4. Extremo Derecha Fondo (Grupo01_c.png) */}
-            <div className="absolute right-[8%] sm:right-[12%] top-[45%] -translate-y-1/2 translate-x-1/2 w-32 h-32 sm:w-48 sm:h-48 md:w-56 md:h-56 opacity-90">
+            {/* 5. Extremo Derecha Fondo (Grupo01_c.png) */}
+            <div 
+              className="absolute right-[8%] sm:right-[12%] top-[45%] -translate-y-1/2 translate-x-1/2 w-32 h-32 sm:w-48 sm:h-48 md:w-56 md:h-56 opacity-75"
+              style={{ filter: "brightness(0.95) contrast(0.90)" }}
+            >
               <div className="w-full h-full animate-sway-right">
                 <img
                   src="/Flores/Grupo01_c.png"
@@ -68,8 +94,11 @@ export default function HeroSection() {
               </div>
             </div>
 
-            {/* 5. Superior Centro Fondo (Grupo02_a.png) */}
-            <div className="absolute top-[5%] left-1/2 -translate-x-1/2 w-36 h-36 sm:w-52 sm:h-52 md:w-60 md:h-60 opacity-85">
+            {/* 6. Superior Centro Fondo (Grupo02_a.png) */}
+            <div 
+              className="absolute top-[5%] left-1/2 -translate-x-1/2 w-36 h-36 sm:w-52 sm:h-52 md:w-60 md:h-60 opacity-70"
+              style={{ filter: "brightness(0.95) contrast(0.90)" }}
+            >
               <div className="w-full h-full animate-sway-delicate">
                 <img
                   src="/Flores/Grupo02_a.png"
@@ -79,8 +108,11 @@ export default function HeroSection() {
               </div>
             </div>
 
-            {/* 6. Inferior Centro Fondo (Grupo02_b.png) */}
-            <div className="absolute bottom-[5%] left-1/2 -translate-x-1/2 w-32 h-32 sm:w-48 sm:h-48 md:w-56 md:h-56 opacity-85">
+            {/* 7. Inferior Centro Fondo (Grupo02_b.png) */}
+            <div 
+              className="absolute bottom-[5%] left-1/2 -translate-x-1/2 w-32 h-32 sm:w-48 sm:h-48 md:w-56 md:h-56 opacity-70"
+              style={{ filter: "brightness(0.95) contrast(0.90)" }}
+            >
               <div className="w-full h-full animate-sway-delicate">
                 <img
                   src="/Flores/Grupo02_b.png"
@@ -114,7 +146,7 @@ export default function HeroSection() {
               ======================================================== */}
           <div className="absolute inset-0 z-20 flex items-center justify-center pointer-events-none">
             
-            {/* 7. Superposición Izquierda sobre la letra 'J' (Grupo02_b.png) */}
+            {/* 8. Superposición Izquierda sobre la letra 'J' (Grupo02_b.png) */}
             <div className="absolute left-[20%] sm:left-[24%] md:left-[26%] bottom-[12%] w-20 h-20 sm:w-28 sm:h-28 md:w-36 md:h-36 opacity-95">
               <div className="w-full h-full animate-sway-left">
                 <img
@@ -125,7 +157,7 @@ export default function HeroSection() {
               </div>
             </div>
 
-            {/* 8. Superposición Derecha sobre la letra 'a' (Grupo02_a.png) */}
+            {/* 9. Superposición Derecha sobre la letra 'a' (Grupo02_a.png) */}
             <div className="absolute right-[18%] sm:right-[22%] md:right-[24%] top-[10%] w-20 h-20 sm:w-28 sm:h-28 md:w-36 md:h-36 opacity-95">
               <div className="w-full h-full animate-sway-right">
                 <img
